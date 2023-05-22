4382d36ad535fe8b8d5d80a5eddb355a29e9beee
zantaclaus <tarzuto@gmail.com>
2023-05-22 21:48:28 +0700

    ✨ Expressions

---
+++ src/main/scala/lectures/part1basics/Expressions.scala
@@ -0,0 +1,52 @@
+package lectures.part1basics
+
+object Expressions extends App {
+
+  val x = 1 + 2 // EXPRESSION
+  println(x)
+
+  println(2 + 3 * 4)
+  // + - * / & | ^ << >> >>>
+
+  println(1 == x)
+  // == != > >= < <=
+
+  println(!(1 == x))
+  // ! && ||
+
+  var aVariable = 2
+  aVariable += 3 // also works with -= *= /=
+  println(aVariable)
+
+  // Instructions (DO) vs Expressions (VALUE)
+
+  // IF expression
+  val aCondition = true
+  val aConditionValue = if(aCondition) 5 else 3 // IF EXPRESSION
+  println(aConditionValue)
+  println(if(aCondition) 5 else 3)
+
+  var i = 0
+  while (i < 10) {
+    println(i)
+    i += 1
+  }
+  // NEVER WRITE THIS AGAIN.
+
+  // EVERYTHING in Scala is an Expression!
+
+  val aWeirdValue = (aVariable = 3) // Unit === void
+  println(aWeirdValue)
+
+  // side effects: println(), whiles, reassigning
+
+  // Code blocks
+  val aCodeBlock = {
+    val y = 2
+    val z = y + 1
+
+    if (z > 2) "hello" else "goodbye"
+  }
+  // The value of code block is that value of LAST EXPRESSION
+
+}
