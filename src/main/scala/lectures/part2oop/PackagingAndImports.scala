75c882bea32b3e9a9b29c2634c0abc96141048d5
zantaclaus <tarzuto@gmail.com>
2023-05-28 14:34:34 +0700

    ✨ packaging and imports

---
+++ src/main/scala/lectures/part2oop/PackagingAndImports.scala
@@ -0,0 +1,17 @@
+package lectures.part2oop
+
+import playground.Cinderella
+
+class PackagingAndImports extends App {
+
+  // package members are accessible by their simple name
+  val writer = new Writer("Daniel", "RockTheJVM", 2018)
+
+  // import the package
+  val princess = new Cinderella
+
+  // packages are in hierarchy
+  // matching folder structure.
+
+  // package object
+}
